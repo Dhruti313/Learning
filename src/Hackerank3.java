b0e90f8be4f2674f048c6b911ba5f6ba3a26d89f
Dhruti313 <124250238+Dhruti313@users.noreply.github.com>
2023-04-04 21:47:06 +0530

    printf stmtement

--- src/Hackerank3.java
+++ src/Hackerank3.java
@@ -13,7 +13,7 @@ import java.io.*;
 //}
 public class Hackerank3 {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World 123");
 
 
     }
